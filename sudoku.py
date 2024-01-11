bd5f2e14afb35fd529f651ae0009c71ca74fcbb0
aliensbright <144503232+aliensbright@users.noreply.github.com>
2024-01-11 12:51:47 -0800

    Adding lines still

--- sudoku.py
+++ sudoku.py
@@ -16,20 +16,31 @@ values=[0,8,0,0,0,0,0,3,0,2,0,0,6,0,7,0,0,1,0,0,0,0,0,0,0,0,0,0,6,0,2,0,1,0,7,0,
 
 board=[]
 
-
-#def boardsetup(values,board):
-#    for num in values:
-#        if num!=0:
-#            board.append(tk.Label(text=num,width=2,bg='#ffffff',height=1,fg='#0000ff'))
-#        else:
-#            board.append(tk.Entry(text='',width=3,borderwidth=0))
-#    for bored in board:
-#        x=(board.index(bored))%9
-#        y=math.floor((board.index(bored)/9))
-#        bored.grid(row=y,column=x)
-    
-
-    
+#def grid(lines):
+#    for gridLines in lines:
+#        print(gridLines)
+#        gridLines[0].place(x=gridLines[1],y=gridLines[2])
+def boardsetup(values,board):
+    for num in values:
+        if num!=0:
+            board.append(tk.Label(text=num,relief=RIDGE,width=2,bg='#ffffff',border=2,height=1,fg='#0000ff'))
+        else:
+            board.append(tk.Entry(justify=CENTER,width=3,borderwidth=2,relief=RIDGE))
+    for bored in board:
+        x=(board.index(bored))%9+1
+        y=math.floor((board.index(bored)/9))+1
+        vert=0
+        horz=0
+        print(x,y,bored)
+        if x%3==0:
+            vert=4
+            print(vert)
+        if y%3==0:
+            horz=4
+            print(horz)
+        bored.grid(row=y,column=x,padx=vert,pady=horz)
+   
+   
 #0,8,0,0,0,0,0,3,0
 #2,0,0,6,0,7,0,0,1
 #0,0,0,0,0,0,0,0,0
@@ -39,15 +50,14 @@ board=[]
 #4,0,1,3,0,9,7,0,6
 #0,2,0,0,0,0,0,1,0
 #8,0,3,1,0,6,5,0,9
-#boardsetup(values,board)
+boardsetup(values,board)
 
-hor1=hor2=hor3=hor4=hor5=hor6=hor7=hor8=Label(image=(PhotoImage(file='Images/HorizontalLine.png',height=1)))
-verLine=Label(image=(PhotoImage(file='Images/VerticalLine.png')))
-
-lines=((hor1,100,100),(hor2,200,200),(hor3,300,300),(hor4,400,0),(hor5,500,0),(hor6,600,0),(hor7,700,0),(hor8,800,0))
-
-for gridLines in lines:
-    print(gridLines)
-    gridLines[0].place(y=gridLines[1],x=gridLines[2])
+#hor1=hor2=hor3=hor4=hor5=hor6=hor7=hor8=Label(image=(PhotoImage(file='Images/HorizontalLine.png',height=1)))
+#verLine=Label(image=(PhotoImage(file='Images/VerticalLine.png')))
+#
+#lines=((hor1,-100,20),(hor2,200,200),(hor3,300,300),(hor4,400,0),(hor5,500,0),(hor6,600,0),(hor7,700,0),(hor8,800,0))
+#
+#grid(lines)
+#
 
 win.mainloop()
