25a1d0dcf045ae71958acc5e6d2a0686c9ecd90c
aliensbright <144503232+aliensbright@users.noreply.github.com>
2024-01-11 13:29:09 -0800

    Done Lines

--- sudoku.py
+++ sudoku.py
@@ -10,16 +10,14 @@ from tkinter import ttk
 import math
 
 win=tk.Tk()
-win.config(bg="#0000ff")
-values=[0,8,0,0,0,0,0,3,0,2,0,0,6,0,7,0,0,1,0,0,0,0,0,0,0,0,0,0,6,0,2,0,1,0,7,0,5,0,0,0,0,0,0,0,3,9,0,0,7,0,5,0,0,8,4,0,1,3,0,9,7,0,6,0,2,0,0,0,0,0,1,0,8,0,3,1,0,6,5,0,9]
-
-
+win.config(bg="#000000")
+variablevalues=ogvalues=[0,8,0,0,0,0,0,3,0,2,0,0,6,0,7,0,0,1,0,0,0,0,0,0,0,0,0,0,6,0,2,0,1,0,7,0,5,0,0,0,0,0,0,0,3,9,0,0,7,0,5,0,0,8,4,0,1,3,0,9,7,0,6,0,2,0,0,0,0,0,1,0,8,0,3,1,0,6,5,0,9]
+checkbutton=tk.Button(text='Check')
+Title=Label(text='Sudoku')
 board=[]
+Title.grid(row=0,column=0,columnspan=10)
+checkbutton.grid(row=0,column=7,columnspan=3)
 
-#def grid(lines):
-#    for gridLines in lines:
-#        print(gridLines)
-#        gridLines[0].place(x=gridLines[1],y=gridLines[2])
 def boardsetup(values,board):
     for num in values:
         if num!=0:
@@ -34,13 +32,15 @@ def boardsetup(values,board):
         print(x,y,bored)
         if x%3==0:
             vert=4
-            print(vert)
         if y%3==0:
             horz=4
-            print(horz)
-        bored.grid(row=y,column=x,padx=vert,pady=horz)
-   
-   
+        bored.grid(row=y,column=x,padx=(0,vert),pady=(0,horz))
+ 
+def checkAnswer(event):
+    global variablevalues
+
+    
+
 #0,8,0,0,0,0,0,3,0
 #2,0,0,6,0,7,0,0,1
 #0,0,0,0,0,0,0,0,0
@@ -50,14 +50,5 @@ def boardsetup(values,board):
 #4,0,1,3,0,9,7,0,6
 #0,2,0,0,0,0,0,1,0
 #8,0,3,1,0,6,5,0,9
-boardsetup(values,board)
-
-#hor1=hor2=hor3=hor4=hor5=hor6=hor7=hor8=Label(image=(PhotoImage(file='Images/HorizontalLine.png',height=1)))
-#verLine=Label(image=(PhotoImage(file='Images/VerticalLine.png')))
-#
-#lines=((hor1,-100,20),(hor2,200,200),(hor3,300,300),(hor4,400,0),(hor5,500,0),(hor6,600,0),(hor7,700,0),(hor8,800,0))
-#
-#grid(lines)
-#
-
+boardsetup(ogvalues,board)
 win.mainloop()
